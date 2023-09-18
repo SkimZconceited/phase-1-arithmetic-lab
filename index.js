0d9e01970d4bcf03a1be3318681ef779b731fe77
Stanley 'SkimZ' Kimambo <Stanleykimambo@gmail.com>
2023-09-19 00:51:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
+
+let random = 0;
+random = Number.isInteger(Math.random()) + 1;
+
+let num3 = 16;
+let num4 = 6;
+let mod = 0;
+
+mod = num3 % num4;
+
+let max = 0;
+max = Math.max(1, 14, 20, 19, 11, 7, 3);
